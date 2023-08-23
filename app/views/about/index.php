4bf0534a48d62528e584f2477d854bc88b4802c9
unknown <rizqirenaldy06@gmail.com>
2023-08-23 10:24:18 +0800

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/iky.png" alt="Iky"width="200" class="rounded-circle shadow">
+    <p>Hallo,Nama saya <?= $data['nama'];?>, Umur Saya <?= $data['umur'];?> 
+    tahun, Saya adalah seorng <?= $data['pekerjaan'];?>.</p>
+</div>
+    
+
